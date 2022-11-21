2340ad21529e8a85de1934e9b1d2550d98ce74b5
“yiti” <729390400@qq.com>
2022-11-21 16:43:55 +0800

    test

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,7 @@ import { foodCommand } from './template/food';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+	//test
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "nlswebpack" is now active!');
